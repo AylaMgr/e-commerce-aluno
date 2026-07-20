89d761eea9992efde7777df7247396c26e063ceb
Matheus de Sousa dos Santos <matheusmtsm75@gmail.com>
2026-07-20 19:13:29 -0300

    Segundo Commit

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -3,11 +3,12 @@ import { Produto } from '../produto/produto';
 import { signal } from '@angular/core';
 import { computed } from '@angular/core';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
-
+import { effect } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto, PrecoFormatadoPipe],
+  imports: [Produto, PrecoFormatadoPipe, UpperCasePipe],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
@@ -38,6 +39,7 @@ export class ListaProdutos {
 //!Função para exibir produtos selecionados pelo usuario no console
   exibirProduto (nome: string){
     console.log ('Produto Selecionado: ', nome);
+    this.produtoSelecionado.set(nome);
   }
   //! função que adicionar produto usando metodo update()
   adicionarProduto(){
@@ -51,4 +53,31 @@ export class ListaProdutos {
   valorTotal = computed(() =>
   {return this.produtos().reduce((total, item) =>
   total + item.preco,0)});
+  //!função para substituir a lista atual usando o metodo set()
+  substituirProdutos(){
+    this.produtos.set([
+      { nome:'Teclado', preco: 50 },
+      { nome:'Mouse', preco: 15 },
+      { nome:'Monitor', preco: 500 },
+      { nome:'Desktop', preco: 1500 },
+      { nome:'headset', preco: 30 },
+    ]);
+  }
+//! metodo para monitorar alterações em tempo real usando effect() 
+constructor(){
+  effect(() => {
+    console.log('Lista de Produtos Alterados: ', this.produtos());
+  });
+  effect(() => {
+    console.log('Valor Total Atualizado: ', this.valorTotal());
+  });
+  effect(() => {
+    if (typeof document !== 'undefined'){
+      document.title = `(${this.totalProdutos()}) - Loja do Matheus`;
+    }
+  });
+}
+//! Metodo para criar um estado de seleção com signal string | null
+produtoSelecionado = signal <string | null>(null);
+
 }
