0a00f71446936dd29722fbe5b8a3ac3e6a77c693
Ayla Pessoa <aylamargarida21@gmail.com>
2026-07-21 21:25:44 -0300

    feat(routes): configurar rotas para Home, Produtos, Carrinho e direcionamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from '@angular/router';
 import {ListaProdutos} from './features/produtos/lista-produtos/lista-produtos';
 import { Carrinho } from './feactures/carrinho/carrinho/carrinho';
+import {Home} from './features/home/home/home';
 
 export const routes: Routes = [
     {
 path:'',
-component: ListaProdutos,
+component: Home,
 },
+{path:'produtos',
+    component: ListaProdutos,},
 {path:'carrinho',
-    component: Carrinho},
+    component: Carrinho
+},
+{path:'**',
+    redirectTo:'',
+},
 ];
