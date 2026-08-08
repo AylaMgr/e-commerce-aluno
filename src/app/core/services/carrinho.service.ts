5392a73257f1ff7a78a44f85d879be2035eaa68a
Ayla Pessoa <aylamargarida21@gmail.com>
2026-08-07 21:16:07 -0300

    refactor(produtos): mover produtos.service para pasta core/services

---
+++ src/app/core/services/carrinho.service.ts
@@ -0,0 +1,25 @@
+import {Injectable, signal, computed} from '@angular/core';
+
+@Injectable({providedIn: 'root'})
+
+export class carrinhoService {
+    //!estado global do carrinho
+    private carrinho = signal<{nome: string; preco: number}[]>([]);
+
+    //? seleção de produtos do carrinho
+    itens = computed(() =>
+        this.carrinho());
+    quantidadeItens = computed(() =>
+        this.carrinho().length);
+    totalItens = computed(() =>
+        this.carrinho().reduce((total, item) =>total + item.preco, 0));
+
+    // TODO: ação de adicionar produto ao carrinho
+    adicionar(produto:{nome: string; preco: number}){
+    this.carrinho.update( lista => [...lista, produto]);
+}
+    //TODO: ação de limpar o carrinho
+    limpar(){
+        this.carrinho.set([]);
+}
+}
