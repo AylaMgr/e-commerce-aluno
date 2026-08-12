c9db071d8ea0e6118b35d647fccee093b97a4055
Ayla Pessoa <aylamargarida21@gmail.com>
2026-08-12 20:53:56 -0300

    feat(carrinho): adicionar condicao para verificar e retornar estado de carrinho vazio

--- src/app/core/services/carrinho.service.ts
+++ src/app/core/services/carrinho.service.ts
@@ -1,10 +1,15 @@
 import {Injectable, signal, computed} from '@angular/core';
 
+type ItemCarrinho ={
+    nome: string;
+    preco: number;
+}
+
 @Injectable({providedIn: 'root'})
 
 export class CarrinhoService {
     //!estado global do carrinho
-    private carrinho = signal<{nome: string; preco: number}[]>([]);
+    private carrinho = signal<ItemCarrinho[]>([]);
 
     //? seleção de produtos do carrinho
     itens = computed(() =>
@@ -13,9 +18,11 @@ export class CarrinhoService {
         this.carrinho().length);
     totalItens = computed(() =>
         this.carrinho().reduce((total, item) =>total + item.preco, 0));
+    carrinhoVazio = computed(() =>
+        this.carrinho().length === 0);
 
     // TODO: ação de adicionar produto ao carrinho
-    adicionar(produto:{nome: string; preco: number}){
+    adicionar(produto: ItemCarrinho){
     this.carrinho.update( lista => [...lista, produto]);
 }
     //TODO: ação de limpar o carrinho
