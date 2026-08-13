408751153665c5c0c92382ba45cd297c5be71cc9
Ayla Pessoa <aylamargarida21@gmail.com>
2026-08-12 21:30:34 -0300

    feat(auth): criar servico auth.service.ts para gerenciamento de autenticacao

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,6 +1,12 @@
 import {CanActivateFn, Router} from '@angular/router';
-import {usuarioLogado} from './auth';
+import { AuthService } from './services/auth.service';
+import { inject } from '@angular/core';
 //condição para acessar a rota, se o usuário estiver logado, ele pode acessar a rota, caso contrário, ele será redirecionado para a página de login
 export const authGuard: CanActivateFn = () => {
-    return usuarioLogado();
+    const authService = inject (AuthService);
+    const router = inject (Router)
+if(authService.usuarioLogado()){
+    return true;
+}
+return router.createUrlTree(['/login']);
 };
