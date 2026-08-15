00d5e9da513c78caa8f97773cd9bc12f8759e1c1
Ayla Pessoa <aylamargarida21@gmail.com>
2026-08-14 21:19:41 -0300

    fix(interceptor): corrigir chamada do metodo obterToken no AuthService

--- src/app/core/interceptors/http.interceptor.ts
+++ src/app/core/interceptors/http.interceptor.ts
@@ -2,16 +2,22 @@ import {HttpInterceptorFn } from '@angular/common/http';
 import { tap } from 'rxjs';
 import { catchError } from 'rxjs';
 import { throwError } from 'rxjs';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
     
-    console.log('Interceptando Requisição:', req.url);
-    const token = 'fake-token-jwt'; // Simulando um token JWT
-    const novareq = req.clone({
-        setHeaders: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
+    console.log('REQUEST', req.url);
+    const authService = inject(AuthService);
+    const router = inject(Router);
+    const token = authService.obterToken();
+    const novareq = token ?
+        req.clone({
+            setHeaders: {
+                Authorization: `Bearer ${token}`,
+            },
+        }): req;
 return next(novareq).pipe (
     tap({
         next: (event) => console.log('Respone ', event),
@@ -22,12 +28,17 @@ return next(novareq).pipe (
         return throwError(() => error);
     }),
     catchError((error) => {
-        console.error('ERRO GLOBAL:', error);
-        if (error.status === 401) {
-            console.warn('Usuario não autorizado!');
+        if(error.status ===401) {
+            console.error('Erro de autenticação de Usuario', error);
+            authService.logout();
+            router.navigateByUrl('/login');
         }
         if (error.status === 500) {
-            console.warn('Erro interno do servidor!');
+            console.warn('Erro interno do servidor!', error);
+        }
+        if(error.status === 403){
+            console.warn('Acesso Proibido, Usuario sem Autorização'),
+            router.navigateByUrl('/produtos');
         }
         return throwError(() => error);
     }),
