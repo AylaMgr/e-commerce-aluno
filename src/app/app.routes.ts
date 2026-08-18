1a78db4d7509876f89989e198116e66e9c6e43f1
Ayla Pessoa <aylamargarida21@gmail.com>
2026-08-17 21:50:13 -0300

    feat(routes): configurar rotas de autenticacao e protecao por guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -33,6 +33,11 @@ export const routes: Routes = [
         loadComponent:() => import('./features/login/login/login').then((m) => m.Login),
     },
     {
+        path: 'acesso-negado',
+        loadComponent: () =>
+            import('./features/acesso-negado/acesso-negado/acesso-negado').then((m) => m.AcessoNegado),
+    },
+    {
         path: 'admin',
         canActivate: [adminGuard],
         loadComponent: () =>
